1f94a4f49389622aaded0e7ab112269545cb49e4
Nour Aswad <nouraswad@hotmail.com>
2017-02-09 18:01:10 +0000

    completed code and tests for indexOf and initial code and test for filter

--- lowbar.js
+++ lowbar.js
@@ -16,6 +16,51 @@ _.first = function(arr, n) {
 };
 
 
+_.last = function(arr,n) {
+  if(!arguments.length || arr.length === 0) {
+  return undefined;
+}
+if(arguments.length === 1) {
+  return arr[arr.length - 1];
+}else{
+  return arr.slice(n-1)
+}
+
+};
+
+_.each = function() {
+
+
+};
+
+_.indexOf = function(arr,val) {
+  if(arguments.length === 2){
+    for(var i=0;i<arr.length;i++){
+      if(arr[i] === val){
+        return arr.indexOf(val)
+      }
+    }
+  }else{
+  return -1;
+  }
+};
+
+_.filter = function(list,condition) {
+  if(!arguments.length || typeof arguments[0]=== 'number'){
+    return [];
+  }
+  if(arguments.length === 1){
+    return list;
+  }
+
+
+};
+
+
+
+
+
+
 if (typeof module !== 'undefined') {
   module.exports = _;
 }
--- spec/lowbar.spec.js
+++ spec/lowbar.spec.js
@@ -49,6 +49,96 @@ describe('_', function () {
     expected = undefined;
     expect(actual).to.eqls(expected); 
   });
+describe('#last', function () {
+    it('is a function', function() {
+      expect(_.last).to.be.a('function');
+    });
+it('should return the last element', function(){
+    var actual = _.last([1, 2, 3]);
+    var expected = 3;
+    expect(actual).to.equal(expected); 
+    actual = _.last('hello');
+    expected = 'o'; 
+    expect(actual).to.equal(expected);
+  }); 
+
+   it('should return the last n elements if n is passed', function(){
+    var actual = _.last([1, 2, 3, 4, 5], 3);
+    var expected = [3, 4, 5];
+    expect(actual).to.eqls(expected); 
+  });
+  it('should return undefined if no argument or an empty array is passed', function(){
+    var actual = _.last();
+    var expected = undefined;
+    expect(actual).to.eqls(expected); 
+    actual = _.last([]);
+    expected = undefined;
+    expect(actual).to.eqls(expected); 
+  });
+  });
+  describe('#each', function () {
+    it('is a function', function() {
+      expect(_.each).to.be.a('function');
+    });
+  });
+
+
+describe('#indexOf', function () {
+    it('is a function', function() {
+      expect(_.indexOf).to.be.a('function');
+    });
+    it('should return -1 unless the val is found in the array', function(){
+    var actual = _.indexOf();
+    var expected = -1;
+    expect(actual).to.eqls(expected); 
+    actual = _.indexOf([]);
+    expected = -1;
+    expect(actual).to.eqls(expected); 
+  });
+it('should return index at which the val is found in the array', function(){
+    var actual = _.indexOf([1,2,3],2);
+    var expected = 1;
+    expect(actual).to.eqls(expected); 
+    actual = _.indexOf([1,1,2,2,2,3],2);
+    expected = 2;
+    expect(actual).to.eqls(expected); 
+    actual = _.indexOf(['a','b','c'],'c');
+    expected = 2;
+    expect(actual).to.eqls(expected); 
+    actual = _.indexOf(['a','a','a','b','c'],'a');
+    expected = 0;
+    expect(actual).to.eqls(expected); 
+  });
+
+  });
+
+
+  describe('#filter', function () {
+    it('is a function', function() {
+      expect(_.filter).to.be.a('function');
+    });
+    it('should return an empty array if passed a number or nothing', function(){
+    var actual = _.filter();
+    var expected = [];
+    expect(actual).to.eqls(expected); 
+    actual = _.filter(989);
+    expected = [];
+    expect(actual).to.eqls(expected);
+    actual = _.filter([1,2,3]);
+    expected = [1,2,3];
+    expect(actual).to.eqls(expected);  
+  });
+
+
+  });
+  
+  
+
+
+
+
+
+  
 
   
 });
